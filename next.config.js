06d36761a529b1768b640950190d4bbb1477097b
finzxs <haydenfinney@usf.edu>
2025-11-09 14:56:06 -0500

    typescript cleaning and page.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// next.config.js
+const nextConfig = { reactStrictMode: true };
+module.exports = nextConfig;
